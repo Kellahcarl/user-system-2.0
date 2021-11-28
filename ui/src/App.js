2e3fe49ac693ad7a77eb8c7d4ed0d9f3e78864f3
pimby <baraka608@student.mmarau.ac.ke>
2021-11-28 13:23:19 +0300

    added user reg login dashboard

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import "./App.css";
+
+import Login from "./pages/login";
+
+import Reg from "./pages/reg";
+
+import Dashboard from "./pages/dashboard";
+
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="container">
+        <nav className="navbar navbar-expand-lg navheader">
+          <div className="collapse navbar-collapse">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item">
+                <Link to={"/Login"} className="nav-link">
+                  Login
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <Link to={"/Signup"} className="nav-link">
+                  Sign Up
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </nav>{" "}
+        <br />
+        <Switch>
+          <Route exact path="/Login" component={Login} />
+
+          <Route path="/Signup" component={Reg} />
+        </Switch>
+        <Switch>
+          <Route path="/Dashboard" component={Dashboard} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
